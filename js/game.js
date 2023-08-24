5bfe2e34939a9c8523fb48121fdb79a0c7f366ad
Jiu Yang <g-jiuyang@gashpoint.com>
2023-08-24 18:28:27 +0800

    更新前四個小遊戲

--- js/game.js
+++ js/game.js
@@ -3,25 +3,27 @@ var gameResult = $('.game-zone .play-game .result')
 
 // 開始遊玩
 $('.game-zone .default button').click(function () {
-    $(this).parent().parent().addClass('play')
+    $(this).parent().parent().parent().addClass('play')
 })
 
 
 // 遊玩過程
 gameSelection.find('.play').click(function () {
-    var selectedRadio = gameSelection.find('input[type=radio]:checked');
+    var selectedRadio = gameSelection.find('input[type=radio]:checked')
+    var betsInput = $('.content input[name="bets"]')
 
-    if (selectedRadio.length > 0) {
+    if (selectedRadio.length > 0 && betsInput.val() != '') {
         // 骰子開始滾動
         gameResult.addClass('dicing')
         var imagePath = gameResult.find('img').attr('src')
         var newImagePath = imagePath.replace('png', 'gif')
         gameResult.find('img').attr('src', newImagePath)
+        betsInput.removeClass('alert')
 
-        // 禁用按鈕
-        $(this).attr('disabled', '')
-        gameSelection.find('input').attr('disabled', '')
+        // 遊戲進行中禁用網頁所有操作
+        $('.full-page').addClass('active')
 
+        // 隨機產生三個結果
         var dicePaths = [
             'img/game/dice/dice01.png',
             'img/game/dice/dice02.png',
@@ -30,7 +32,6 @@ gameSelection.find('.play').click(function () {
             'img/game/dice/dice05.png',
             'img/game/dice/dice06.png'
         ];
-
         var selectedDicePaths = [];
         while (selectedDicePaths.length < 3) {
             var randomIndex = Math.floor(Math.random() * dicePaths.length);
@@ -38,25 +39,38 @@ gameSelection.find('.play').click(function () {
                 selectedDicePaths.push(dicePaths[randomIndex])
             }
         }
-
-        // 隨機產生三個結果
+        // 摋動2秒後出現結果
         setTimeout(() => {
             gameResult.removeClass('dicing')
             gameResult.addClass('show')
             gameResult.find('img').each(function (index) {
                 $(this).attr('src', selectedDicePaths[index])
             })
-        }, 3000);
+        }, 2000);
 
         // 彈窗顯示遊戲結果
         setTimeout(() => {
             $('#game-notice').addClass('active')
-        }, 5000);
+            $('.full-page').removeClass('active')
+        }, 4000);
     } else {
-        $(this).siblings('div').addClass('alert');
-        setTimeout(() => {
-            $(this).siblings('div').removeClass('alert');
-        }, 1000);
+        // 沒選賭注時跳提醒
+        if (selectedRadio.length === 0) {
+            $(this).siblings('div').addClass('alert');
+            setTimeout(() => {
+                $(this).siblings('div').removeClass('alert');
+            }, 1000);
+        }
+
+        // 沒填賭本時跳提醒
+        if (betsInput.val() === '') {
+            betsInput.removeClass('alert')
+            setTimeout(() => {
+                betsInput.addClass('alert')
+            }, 1);
+        } else {
+            betsInput.removeClass('alert')
+        }
     }
 })
 
@@ -65,10 +79,8 @@ gameSelection.find('.play').click(function () {
 $('.replay').click(function () {
     $('.popUp').removeClass('active')
     gameSelection.find('input[type="radio"]').prop('checked', false)
-    gameSelection.find('button').attr('disabled', false)
-    gameSelection.find('input').attr('disabled', false)
     gameResult.removeClass('show')
-    gameResult.find('img:first-child').attr('src', 'img/game/dice/dice01.png')
+    gameResult.find('img:nth-child(1)').attr('src', 'img/game/dice/dice01.png')
     gameResult.find('img:nth-child(2)').attr('src', 'img/game/dice/dice02.png')
     gameResult.find('img:nth-child(3)').attr('src', 'img/game/dice/dice03.png')
 })
