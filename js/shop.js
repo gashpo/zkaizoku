43d97f77dbec7a6fd1e3ec97dd3e75b6df808a56
Jiu Yang <g-jiuyang@gashpoint.com>
2023-09-12 11:59:31 +0800

    建立 v3.12 版本

--- js/shop.js
+++ js/shop.js
@@ -5,22 +5,22 @@ $('.step1 .submit').click(function () {
     progress.find('li:first-child h5').html('<i class="icon-Check"></i>')
     progress.removeClass('step1')
     progress.addClass('step2')
-    $('.product-detail.step1').css('display', 'none')
-    $('.product-detail.step2').removeAttr('style')
+    $('.product-detail.step1').removeClass('active')
+    $('.product-detail.step2').addClass('active')
 })
 
 $('.step2 .prev-step').click(function () {
     progress.find('li:first-child h5').html('1')
     progress.removeClass('step2')
     progress.addClass('step1')
-    $('.product-detail.step2').css('display', 'none')
-    $('.product-detail.step1').removeAttr('style')
+    $('.product-detail.step2').removeClass('active')
+    $('.product-detail.step1').addClass('active')
 })
 
 $('#failed .submit').click(function () {
     progress.find('li:nth-child(2) h5').html('<i class="icon-Check"></i>')
     progress.removeClass('step2')
     progress.addClass('step3')
-    $('.product-detail.step2').css('display', 'none')
-    $('.product-detail.step3').removeAttr('style')
+    $('.product-detail.step2').removeClass('active')
+    $('.product-detail.step3').addClass('active')
 })
